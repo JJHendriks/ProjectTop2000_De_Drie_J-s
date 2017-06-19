5222c0cf5cfa926117da66b0f089a94d3dde77ee
Jim Hendriks <jjhendriks@outlook.com>
2017-06-19 14:41:54 +0200

    Kut onedrive

---
+++ Top2000/BusinessLayer/Song.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+   public class Song
+    {
+        private string song_ID;
+
+        public string Song_ID 
+        {
+            get { return song_ID; }
+            set { song_ID = value; }
+        }
+
+        private string artist_ID;
+
+        public string Artist_ID
+        {
+            get { return artist_ID; }
+            set { artist_ID = value; }
+        }
+
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        private string year;
+
+        public string Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private string image;
+
+        public string Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        private string extra_Info;
+
+        public string Ex
+        {
+            get { return extra_Info; }
+            set { extra_Info = value; }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Top2000/BusinessLayer/Toplist_Song.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Toplist_Song
+    {
+        private string toplist_Year;
+
+        public string Toplist_Year
+        {
+            get { return toplist_Year; }
+            set { toplist_Year = value; }
+        }
+
+        private string song_ID;
+
+        public string Song_ID
+        {
+            get { return song_ID; }
+            set { song_ID = value; }
+        }
+
+        private int rank;
+
+        public int Rank
+        {
+            get { return rank; }
+            set { rank = value; }
+        }
+
+
+
+    }
+}
