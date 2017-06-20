f976ab6388bd9fa1e74a225667d9875c4682e428
Jim Hendriks <jjhendriks@outlook.com>
2017-06-20 19:37:40 +0200

    Hallo

--- Top2000/BusinessLayer/ListProvider.cs
+++ Top2000/BusinessLayer/ListProvider.cs
@@ -9,5 +9,72 @@ namespace BusinessLayer
    public static class ListProvider
     {
 
+
+
+
+
+        public static List<Admin> Admins;
+        /// <summary>
+        /// Deze methode maakt alle admins aan
+        /// </summary>
+        public static void LijstAdminsVullen()
+        {
+            if (Admins == null)
+            {
+                Admins = new List<Admin>();
+
+
+                Admins.Add(new Admin
+                    (
+                    _gebruikersnaam: "jimhendriks",
+                    _wachtwoord: "wachtwoord"
+                    ));
+
+                Admins.Add(new Admin
+                    (
+                    _gebruikersnaam: "gebruiker",
+                    _wachtwoord: "wachtwoord"
+                    ));
+
+                Admins.Add(new Admin
+                    (
+                    _gebruikersnaam: "gebruiker2",
+                    _wachtwoord: "wachtwoord2"
+                    ));
+
+            }
+
+        }
+        /// <summary>
+        /// Deze methode haalt een admin uit de lijst doormiddel van de gebruikersnaam en het wachtwoord
+        /// </summary>
+        /// <param name="_gebruikersnaam"></param>
+        /// <param name="_wachtwoord"></param>
+        /// <returns></returns>
+        public static Admin Inloggen(string _gebruikersnaam, string _wachtwoord)
+        {
+            LijstAdminsVullen();
+            var list = Admins;
+
+            foreach (var item in list)
+            {
+                if (_gebruikersnaam == item.Gebruikersnaam)
+                {
+                    if (_wachtwoord == item.Wachtwoord)
+                    {
+                        return item;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Uw wachtwoord klopt niet");
+                    }
+                }
+
+            }
+            throw new ArgumentException("Uw gebruikersnaam bestaat niet");
+
+
+        }
     }
 }
+
